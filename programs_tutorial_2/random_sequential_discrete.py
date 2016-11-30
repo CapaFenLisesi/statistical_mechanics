1bc7a43c66cbcea897b481701cc9a9e629ebe9f2
trap_beast <trap_beast@mines.edu>
2016-11-30 10:02:52 -0700

    init for week 4 programs

--- programs_tutorial_2/random_sequential_discrete.py
+++ programs_tutorial_2/random_sequential_discrete.py
@@ -3,7 +3,7 @@ import random
 configurations = {(0, 3): 'a', (0, 4): 'b', (1, 4): 'c', 
                   (3, 0): 'd', (4, 0): 'e', (4, 1): 'f'}
 counts = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0}
-n_runs = 10000
+n_runs = 100000
 for i in range(n_runs):
     red_rod = random.randint(0, 3)
     if red_rod >= 2: red_rod += 1
